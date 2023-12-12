4f880b0df57ce83724c986d92ae77019b47bb289
Ankit Raj <ankitraj102004@gmail.com>
2023-12-12 11:58:43 +0530

    Changed fontsize

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const HistoryItem = ({ele}) => {
   return (
-    <div className='bg-[rgb(255,180,0)] m-2 rounded-lg text-center flex items-center justify-between px-3 gap-1 text-lg py-3 my-2 '>
+    <div onClick={()=>{setClicked((prev)=>!prev)}} className='bg-[rgb(255,180,0)] m-2 rounded-lg text-center flex items-center justify-between px-3 gap-1 text-lg py-3 my-2 '>
       <div>{ele.name}</div>
-      <div className='text-blue-800'>{ele.time}</div>
+      <div className='text-violet-900 text-xl'>{ele.time}</div>
     </div>
   )
 }
